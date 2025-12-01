addc701ca06798ed2a41741a42a738bcd54ad2b2
Jrodolfosilva <rodolfo.silva1602@gmail.com>
2025-11-30 23:35:54 -0300

    feat: Seções, Planta, Implantação, LOcalização, Mapa e Footer -  Desktop

---
+++ assets/scripts/swiper.js
@@ -0,0 +1,61 @@
+
+
+
+
+function SwiperPlantas() {
+    
+  const swiperPlantasThumbs = new Swiper(".swiperPlantasThumbs", {
+    spaceBetween: 10,
+    slidesPerView: 2,
+    /*freeMode: true,
+    watchSlidesProgress: true,
+    watchSlidesVisibility: true,
+    slideToClickedSlide: true,
+    preloadImages: false,
+    lazy: true, */
+
+    // 🔹 Responsividade
+    breakpoints: {
+      0: {
+        slidesPerView: 4,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 15,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
+  });
+
+  const swiperPlantas = new Swiper(".swiperPlantas", {
+    spaceBetween: 10,
+    preloadImages: false,
+    lazy: true,
+    
+    navigation: {
+      nextEl: ".swiper-button-next-plantas",
+      prevEl: ".swiper-button-prev-plantas",
+    },
+    thumbs: { swiper: swiperPlantasThumbs },
+  });
+}
+SwiperPlantas();
+
+
+
+function SwiperImplatacao() {
+ 
+  const swiperImplatacao = new Swiper(".swiperImplatacao", {
+    spaceBetween: 10,
+    preloadImages: false,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
+SwiperImplatacao();
